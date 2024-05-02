0973ca605dc403e67a0da2a9be20b7c6dca2f14c
Isabella <collantemendezisabella@gmail.com>
2024-05-02 14:14:36 -0500

    first commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,6 @@
+// interface JSX.IntrinsicElements {
+//     div: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+//     h1: React.DetailedHTMLProps<React.HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>;
+//     // Agrega más elementos aquí si los necesitas
+// }
+/// <reference types="vite/client" />
